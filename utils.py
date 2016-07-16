0b8446c860736febd60bd2c1c3c58f608553b36c
David Slater <dsslater@stanford.edu>
2016-07-16 13:34:43 -0700

    dded Event Handlers

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-class stock:
+class Stock:
 	name = ""
 	ETF = False
 	members = []
@@ -6,18 +6,31 @@ class stock:
 	buyPrice = 0
 	def __init__(self, name, ETF = False, members = [], sellPrice = 0, buyPrice = 0)
 
+class Event:
+	triggerStocks = []
+	testFunc = None 
+	actionFunc = None
+	def __init__(self, triggerStocks, testFunc, actionFunc):
+		self.triggerStocks = triggerStocks
+		self.testFunc = testFunc
+		self.actionFunc = actionFunc
+
 class Prices:
 	stocks = {}
+	stockEvents = {}
+	exchange = None
 	def setStockSell(self, name, sellPrice):
 		if name in self.stocks:
 			self.stocks[name].sellPrice = sellPrice
 		else :
-			self.stocks[name] = stock(name, sellPrice=sellPrice)
+			self.stocks[name] = Stock(name, sellPrice=sellPrice)
+		self.checkEvents(stocks[name])
 	def setStockBuy(self, name, buyPrice):
 		if name in self.stocks:
 			self.stocks[name].buyPrice = buyPrice
 		else :
-			self.stocks[name] = stock(name, buyPrice=buyPrice)
+			self.stocks[name] = Stock(name, buyPrice=buyPrice)
+		self.checkEvents(stocks[name])
 	def addNewStock(stock):
 		self.stocks[stock.name] = stock
 	def getStockSell(name):
@@ -28,7 +41,19 @@ class Prices:
 		return self.stocks[name].ETF
 	def getMembers(name):
 		return self.stocks[name].members
-	def __init__(self):
+	def registerEvent(self, event):
+		for stockName in event.triggerStocks:
+			if stockName in self.stockEvents:
+				self.stockEvents[stockName].append(event)
+			else :
+				self.stockEvents[stockName] = [event]
+	def checkEvents(stock)
+		if stock.name in self.stockEvents:
+			for event in self.stockEvents[stock.name]:
+				if event.testFunc(self):
+					event.actionFunc(self)
+	def __init__(self, exchange):
+		self.exchange = exchange
 		self.stocks["BOND"] = stock("BOND")
 		self.stocks["VALBZ"] = stock("VALBZ", True, [("VALE", 1)])
 		self.stocks["VALE"] = stock("VALE", True, [("VALBZ", 1)])
@@ -50,3 +75,27 @@ def processBookJSON(msg, prices):
 def processMsg(msg, prices):
 	if msg.type == "book":
 		processBookJSON(msg, prices)
+
+def testFunc(prices):
+	return True
+
+def buyBonds(prices):
+	print("BUYING", file=prices.exchange)
+
+def registerStockEvents(prices):
+	prices.registerEvent(["BOND"], testFunc, buyBonds)
+
+
+def demo():
+	prices = Prices()
+	registerStockEvents(prices)
+	while True:
+		msg = getServerMsg()
+		processMsg(msg)
+
+"""
+Get server message
+updateDB
+check if action is available
+take action
+"""
