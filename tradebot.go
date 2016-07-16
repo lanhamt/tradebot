d5d3655222a5810e071cb6f7e3c9f6f84fce7b7a
lanhamt <tlanham@cs.stanford.edu>
2016-07-16 11:15:00 -0700

    added connection stuff

--- tradebot.go
+++ tradebot.go
@@ -12,7 +12,21 @@ import (
     "io"
     "log"
     "strings"
+    "net"
 )
+
+func handleConnection(c net.Conn) {
+    d : json.NewDecoder(c)
+
+}
+
+func listen() {
+    ln, err := net.Listen("tcp", ":9999")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+}
 
 func main() {
     fmt.Print("tradebot starting up...")
