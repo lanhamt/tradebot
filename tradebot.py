bb19345fa851d3dc075861155ffed4e401d182ae
lanhamt <tlanham@cs.stanford.edu>
2016-07-16 11:54:27 -0700

    quotes

--- tradebot.py
+++ tradebot.py
@@ -8,21 +8,21 @@ import time
 
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("test-exch-SEGFAULT", 20000))
+    s.connect(('test-exch-SEGFAULT', 20000))
     return s.makefile('w+', 1)
 
 def main():
     exchange = connect()
-    print("HELLO TEAMNAME", file=exchange)
+    print('HELLO TEAMNAME', file=exchange)
     hello_from_exchange = exchange.readline().strip()
-    print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+    print('The exchange replied:', hello_from_exchange, file=sys.stderr)
     id_no = 0
     while True:
-    	print("ADD " + str(id_no) + " BOND BUY 999 100", file=exchange)
+    	print('ADD ' + str(id_no) + ' BOND BUY 995 100', file=exchange)
     	id_no += 1
         time.sleep(1)
         response = exchange.readline().strip()
     	print(response)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
