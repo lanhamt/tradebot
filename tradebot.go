7d51bd29721774ecb55d5caaa583bf7b485dac07
lanhamt <tlanham@cs.stanford.edu>
2016-07-16 10:59:22 -0700

    tradebot file added

---
+++ tradebot.go
@@ -0,0 +1,19 @@
+/*
+ * File: tradebot.go
+ * ------------------------------
+ * Tradebot
+ */
+
+package main
+
+import (
+    "fmt"
+    "encoding/json"
+    "io"
+    "log"
+    "strings"
+)
+
+func main() {
+    fmt.Print("tradebot starting up...")
+}
