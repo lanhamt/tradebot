51ba9f1a5a0d05ae70c0c8fc8964e3e0aaeb717c
lanhamt <tlanham@cs.stanford.edu>
2016-07-16 11:35:51 -0700

    added py script

---
+++ tradebot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from __future__ import print_function
+
+import sys
+import socket
+
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("test-exch-SEGFAULT", 20000))
+    return s.makefile('w+', 1)
+
+def main():
+    exchange = connect()
+    print("HELLO TEAMNAME", file=exchange)
+    hello_from_exchange = exchange.readline().strip()
+    print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
