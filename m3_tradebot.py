07af1b99293c21e73e860c9b2a9eefd84f93465a
lanhamt <tlanham@cs.stanford.edu>
2016-07-16 18:30:59 -0700

    portfolio stats

---
+++ m3_tradebot.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+from __future__ import print_function
+
+import sys
+import socket
+import time
+import json
+import thread
+import exp_utils
+import random
+from exp_utils import Order
+from EventFunctions import *
+
+
+def portfolioStats(prices):
+	while True:
+		print('PORTFOLIO STATS:\n')
+		print(prices.portfolio)
+		time.sleep(10)
+
+
+def registerAlgos(prices):
+    prices.registerEvent(exp_utils.Event(['BOND'], dummy, bondBuyExec))
+    prices.registerEvent(exp_utils.Event(['BOND'], dummy, bondSellExec))
+    prices.registerEvent(exp_utils.Event(["XLF","BOND","GS","MS","WFC"], dummy, XLFtoStockTest))
+    prices.registerEvent(exp_utils.Event(["XLF","BOND","GS","MS","WFC"], dummy, StocktoXFLTest))
+    prices.registerEvent(exp_utils.Event(['VALE', 'VALBZ'], dummy, tradeVALEAndVALBZ))
+
+
+def sayHello(exchange):
+    hello = {'type': 'hello', 'team': 'SEGFAULT'}
+    print(json.dumps(hello), file=exchange)
+    hello_from_exchange = exchange.readline().strip()
+    print('The exchange replied:', hello_from_exchange, file=sys.stderr)
+    return json.loads(hello_from_exchange)
+
+
+def trade(exchange):
+    global flowLock
+    initial_balance = sayHello(exchange)
+
+    prices = exp_utils.Prices(exchange, initial_balance)
+    registerAlgos(prices)
+
+    thread.start_new_thread(portfolioStats, (prices,))
+    while True:
+        response = exchange.readline().strip()
+        response = json.loads(response)
+        exp_utils.processMsg(response, prices)
+        if response['type'] != 'book' and response['type'] != 'trade':
+            print(response)
+
+
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('test-exch-segfault', 25000))
+    return s.makefile('w+', 1)
+
+
+def main():
+    print('starting tradebot...')
+    connected = False
+    try:
+        exchange = connect()
+        connected = True
+    except:
+        print('  could not connect, retrying')
+        connected = False
+    if connected:
+        trade(exchange)
+
+
+if __name__ == '__main__':
+    main()
